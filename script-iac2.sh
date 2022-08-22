1ac24880afdee3d6bc793872d07d03dcda07b956
zaffani <guilhermzaffani@gmail.com>
2022-08-22 02:31:51 +0000

    Script automação

---
+++ script-iac2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+@echo "Atualizando Servidor"
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install apache2 -y 
+apt-get install unzip -y
+apt-get install wget -y 
+
+@echo "Baixando arquivos e copiando para o diretorio"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main 
+cp -R ./ * /var/www/html
+
+echo "Finalizado"
+
